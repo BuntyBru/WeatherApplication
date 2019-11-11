b5d008853cd0200603f6f580895b833e883b1841
Buntybru <buntybru@gmail.com>
2019-11-12 00:55:14 +0530

    Added error handling

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const url = 'https://api.darksky.net/forecast/d8931387dcb589c3da3a0bc9afff23b1/3
 const mapURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGl2eWFuc2h1bG9rdHJhIiwiYSI6ImNrMnVwdnZvbjE0NmIzbnRqbTMyd2p5anQifQ.YTnW9LIpAddbadEToxeh-g';
 
 request({url: url, json:true}, (error, response)=>{
-  //  console.log(response.body.currently);
+    if(error)
+    {
+        console.log("The service is unavailable");
+    }
+    else if(response.body.error)
+    {
+        console.log("Something went wrong");
+    }
+    else
+    {
+        console.log(response.body.currently);
+    }
 })
 
 
@@ -12,9 +23,19 @@ request({url: url, json:true}, (error, response)=>{
 
 
 request({url:mapURL, json:true},(error,response)=> {
-
-    const latitude = response.body.features[0].center[1];
-    const longitude = response.body.features[0].center[0];
-    console.log(`Latitude ${latitude} , Longitude ${longitude}`);
+    if(error)
+    {
+        console.log("Error")
+    }
+    else if ( response.body.features.length === 0)
+    {
+        console.log("try again, something went wrong")
+    }
+    else
+    {
+        const latitude = response.body.features[0].center[1];
+        const longitude = response.body.features[0].center[0];
+        console.log(`Latitude ${latitude} , Longitude ${longitude}`);
+    }
 })
 
